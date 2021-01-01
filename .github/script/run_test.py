987d2c9675fb31a92a2a9d96c31bfcd2d15d948d
zmcx16 <zmcx16@gmail.com>
2021-01-02 00:15:01 +0800

    update run_test.py

--- .github/script/run_test.py
+++ .github/script/run_test.py
@@ -19,10 +19,28 @@ def send_get_json(url):
 if __name__ == "__main__":
 
     NORN_SCREENER_LAST_UPDATE_TIME_URL = "https://zmcx16.moe/stock-minehunter/api/task/get-norn-screen-last-update-time"
+    NORN_SCREENER_REPORT_STATUS_URL = "https://zmcx16.moe/stock-minehunter/api/task/get-norn-screen-report-status"
 
     LAST_UPDATE_TIME_SEC_THRESHOLD = 86400
     LAST_UPDATE_TIME_CNT_THRESHOLD = 3000
+    REPORT_STATUS_THRESHOLD = 3000
 
+    TACTIC_LIST = ["BenjaminGraham_v1",
+                "HarryBurnIII_v1",
+                "JamesPOshaughnessy_v1",
+                "JohnNeff_v1",
+                "MichaelBerry_v1",
+                "MichaelPrice_v1",
+                "PeterLynch_v1",
+                "RichardBuchwald_MarvinKline_v1",
+                "RobertSanborn_v1",
+                "Stasistw_v1",
+                "StevenCLeuthold_v1",
+                "TrinityInvestmentManagement_v1",
+                "WarrenEBuffett_v1",
+                "zmcx16_v1"]
+
+    # test last update time
     ret, resp = send_get_json(NORN_SCREENER_LAST_UPDATE_TIME_URL)
     if ret == 0:
         try:
@@ -40,10 +58,23 @@ if __name__ == "__main__":
 
             if pass_count < LAST_UPDATE_TIME_CNT_THRESHOLD or max_sec < LAST_UPDATE_TIME_SEC_THRESHOLD:
                 sys.exit(-2)
-            else:
-                sys.exit(0)
 
         except Exception as ex:
             print('Generated an exception: {ex}, try next target.'.format(ex=ex))
 
-    sys.exit(-1)
+    # test report status by Azure Pipeline to reduce server memory loading
+    """
+    # test report status
+    ret, resp = send_get_json(NORN_SCREENER_REPORT_STATUS_URL)
+    if ret == 0:
+        try:
+            for tactic in TACTIC_LIST:
+                print(f"{tactic}: {resp[tactic]}/{resp['total']}")
+                if resp[tactic] < REPORT_STATUS_THRESHOLD:
+                    sys.exit(-3)
+
+        except Exception as ex:
+            print('Generated an exception: {ex}, try next target.'.format(ex=ex))
+    """
+
+    sys.exit(0)
