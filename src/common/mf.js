5ece55246f512898bbb3927f78bd3934d498594e
zmcx16 <zmcx16@gmail.com>
2021-05-06 01:17:31 +0800

    Add Insider ownership, Insider ownership,Institution ownership, Institution transcations factors

--- src/common/mf.js
+++ src/common/mf.js
@@ -39,13 +39,35 @@ export const MFDataTemplate = {
       name: "DIV_w",
       display_name: "Dividend % weight",
       val: "0.5"
-    }       
+    },
+    {
+      name: "InsiderOwn_w",
+      display_name: "Insider Own weight",
+      val: "0.2"
+    },
+    {
+      name: "InsiderTrans_w",
+      display_name: "Insider Trans weight",
+      val: "0.2"
+    },
+    {
+      name: "InstOwn_w",
+      display_name: "Inst Own weight",
+      val: "0.2"
+    },
+    {
+      name: "InstTrans_w",
+      display_name: "Inst Trans weight",
+      val: "0.2"
+    }    
   ]      
 }
 
 export const MFUrl = "https://project.zmcx16.moe/?page=investment-formula"
 export const MFNote = `
 Ranked the stocks by multiple factors simulatneously: \n
-Value factor: Earnings / Price, Book / Price, Sales / Price \n
+Value factor:  Earnings / Price, Book / Price, Sales / Price\n
 Growth factor: ROE, ROA, ROI\n
+Other factor:  Dividend (%), Insider ownership, Insider ownership,\n
+               Institution ownership, Institution transcations\n
 `
