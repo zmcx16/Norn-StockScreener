3d9d824ec0074557c0701f8141054309e974e04d
zmcx16 <zmcx16@gmail.com>
2021-05-26 02:09:56 +0800

    update ci files

--- .github/fetch_norn_data/fetch_norn_data.py
+++ .github/fetch_norn_data/fetch_norn_data.py
@@ -4,6 +4,7 @@ import json
 import requests
 from datetime import datetime
 
+
 def send_request(url):
     try:
         res = requests.get(url)
@@ -23,18 +24,17 @@ def update_get_market(norn_data_folder_path, config):
     for industry in config['markets']:
         try:
             query_url = base_url + '&industry='+industry
-
             ret, content = send_request(query_url)
-
             if ret == 0:
                 resp = json.loads(content)
                 if resp["ret"] == 0:
                     data = resp["data"]
-                    output = {'update_time': datetime.now(), 'industry': data['industry'], 'symbol': data['symbol'],
-                              'src': data['src'], 'data': data['data']}
+                    for item in data:
+                        output = {'update_time': str(datetime.now()), 'industry': item['industry'], 'symbol': item['symbol'],
+                                  'src': item['src'], 'data': item['data']}
 
-                    with open(readme_path / 'market-industry' / 'market' / data['id'] + '.json', 'w', encoding='utf-8') as f:
-                        f.write(str(output))
+                        with open(norn_data_folder_path / 'market-industry' / 'market' / (item['id'] + '.json'), 'w', encoding='utf-8') as f:
+                            f.write(str(output))
 
                 else:
                     print('server err = {err}, msg = {msg}'.format(err=resp["ret"], msg=resp["err_msg"]))
@@ -46,12 +46,13 @@ def update_get_market(norn_data_folder_path, config):
 
 
 if __name__ == "__main__":
-    # root = pathlib.Path(__file__).parent.resolve()
-    # norn_data_folder_path = root / "norn-data"
-    #config = {}
-    # with open(readme_basic_path, 'r', encoding='utf-8') as f:
-    #    config = json.loads(f.read())
+    root = pathlib.Path(__file__).parent.resolve()
+    norn_data_folder_path = root / ".." / "norn-data"
+    config_path = norn_data_folder_path / "market-industry" / "config.json"
+    config = {}
+    with open(config_path, 'r', encoding='utf-8') as f:
+        config = json.loads(f.read())
 
-    # update_get_market(norn_data_folder_path, config)
-    print('hello world')
+    update_get_market(norn_data_folder_path, config)
+    print('update_get_market done')
 
