cf5291b6c8f12fd2e2e8f38a1301232a135e9272
zmcx16 <zmcx16@gmail.com>
2021-08-31 00:53:15 +0800

    update fetch_gurus_data workflow script

--- .github/fetch_gurus_data/fetch_gurus_data.py
+++ .github/fetch_gurus_data/fetch_gurus_data.py
@@ -67,7 +67,7 @@ def get_gurus(gurus_folder_path):
 
     for manager in managers_list:
         print('get portfolio', manager['id'])
-        output_table["manager_list"].append(manager["name"])
+        output_table["manager_list"].append({'name': manager["name"], 'link': manager["link"]})
         param = {
             'code': token,
             'api': 'get-portfolio',
