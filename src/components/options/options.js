3dcd35e7b2bb64cab1a8098bf9bc1cece64c76d6
zmcx16 <zmcx16@gmail.com>
2022-01-15 01:59:57 +0800

    implement options page

--- src/components/options/options.js
+++ src/components/options/options.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect, useRef } from 'react'
 import Grid from '@material-ui/core/Grid'
+import { DataGrid } from '@material-ui/data-grid'
 import InputLabel from '@material-ui/core/InputLabel'
 import FormControl from '@material-ui/core/FormControl'
 import Select from '@material-ui/core/Select'
 import shortid from 'shortid'
 import useFetch from 'use-http'
+import moment from 'moment'
 
 import ModalWindow from '../modalWindow'
-import { GetDataByFetchObj } from '../../common/reactUtils'
+import DefaultDataGridTable from '../defaultDataGridTable'
+import { GetDataByFetchObj, SymbolNameField, PureFieldWithValueCheck, PercentField, ColorPercentField, ColorPosGreenNegRedField } from '../../common/reactUtils'
 import { Options_Def } from '../../common/optionsDef'
 
 import commonStyle from '../common.module.scss'
@@ -21,6 +24,71 @@ const Options = ({loadingAnimeRef}) => {
     popPureModal: null,
   })
 
+  const tableColList = {
+    Symbol: { hide: false, text: 'Symbol' },
+    StockPrice: { hide: false, text: 'Stock Price' },
+    ExpiryDate: { hide: false, text: 'Expiry Date' },
+    Strike: { hide: false, text: 'Strike' },
+    LastPrice: { hide: false, text: 'Last Price' },
+    AvgEWMA: { hide: false, text: 'Avg Valuation' },
+    PriceBias: { hide: false, text: 'Price Bias' },
+    LastTradeDate: { hide: false, text: 'Last Trade Date' },
+    Bid: { hide: false, text: 'Bid' },
+    Ask: { hide: false, text: 'Ask' },
+    Change: { hide: false, text: 'Change' },
+    PercentChange: { hide: false, text: 'Percent Change' },
+    Volume: { hide: false, text: 'Volume' },
+    OpenInterest: { hide: false, text: 'Open Interest' },
+    ImpliedVolatility: { hide: false, text: 'Implied Volatility' },
+    EWMAHisVol: { hide: false, text: 'EWMA Historical Volatility' },
+    BSM_EWMAHisVol: { hide: false, text: 'Black Scholes Merton' },
+    MC_EWMAHisVol: { hide: false, text: 'Monte Carlo' },
+    BT_EWMAHisVol: { hide: false, text: 'Binomial Tree' },
+  }
+
+  const genTableColTemplate = () => {
+    return [
+      SymbolNameField('symbol', 'Symbol', 130, 'symbol' in hideColState ? hideColState['symbol'] : false),
+      PureFieldWithValueCheck("stockPrice", tableColList.StockPrice.text, 140, 2, "stockPrice" in hideColState ? hideColState["stockPrice"] : tableColList['StockPrice'].hide),
+      {
+        field: 'expiryDate',
+        headerName: 'ExpiryDate',
+        width: 140,
+        type: 'date',
+        renderCell: (params) => (
+          <span>{moment(params.row['expiryDate']).format('YYYY-MM-DD')}</span>
+        ),
+        hide: 'expiryDate' in hideColState ? hideColState['expiryDate'] : tableColList['ExpiryDate'].hide
+      },
+      PureFieldWithValueCheck("strike", tableColList.Strike.text, 140, 2, "strike" in hideColState ? hideColState["strike"] : tableColList['Strike'].hide),
+      PureFieldWithValueCheck("lastPrice", tableColList.LastPrice.text, 140, 2, "lastPrice" in hideColState ? hideColState["lastPrice"] : tableColList['LastPrice'].hide),
+      PureFieldWithValueCheck("avgEWMA", tableColList.AvgEWMA.text, 140, 2, "avgEWMA" in hideColState ? hideColState["avgEWMA"] : tableColList['AvgEWMA'].hide),
+      PureFieldWithValueCheck("priceBias", tableColList.PriceBias.text, 140, 2, "priceBias" in hideColState ? hideColState["priceBias"] : tableColList['PriceBias'].hide),
+      {
+        field: 'lastTradeDate',
+        headerName: 'LastTradeDate',
+        width: 140,
+        type: 'date',
+        renderCell: (params) => (
+          <span>{moment(params.row['lastTradeDate']).format('YYYY-MM-DD')}</span>
+        ),
+        hide: 'lastTradeDate' in hideColState ? hideColState['lastTradeDate'] : tableColList['LastTradeDate'].hide
+      },
+      PureFieldWithValueCheck("bid", tableColList.Bid.text, 140, 2, "bid" in hideColState ? hideColState["bid"] : tableColList['Bid'].hide),
+      PureFieldWithValueCheck("ask", tableColList.Ask.text, 140, 2, "ask" in hideColState ? hideColState["ask"] : tableColList['Ask'].hide),
+      ColorPosGreenNegRedField("change", tableColList.Change.text, 150, 2, "change" in hideColState ? hideColState["change"] : tableColList['Change'].hide, 500),
+      ColorPercentField("percentChange", tableColList.PercentChange.text, 150, 2, "percentChange" in hideColState ? hideColState["percentChange"] : tableColList['PercentChange'].hide, 500),
+      PureFieldWithValueCheck("volume", tableColList.Volume.text, 140, 2, "volume" in hideColState ? hideColState["volume"] : tableColList['Volume'].hide),
+      PureFieldWithValueCheck("openInterest", tableColList.OpenInterest.text, 140, 2, "openInterest" in hideColState ? hideColState["openInterest"] : tableColList['OpenInterest'].hide),
+      PercentField("impliedVolatility", tableColList.ImpliedVolatility.text, 140, 2, "impliedVolatility" in hideColState ? hideColState["impliedVolatility"] : tableColList['ImpliedVolatility'].hide),
+      PercentField("EWMAHisVol", tableColList.EWMAHisVol.text, 140, 2, "EWMAHisVol" in hideColState ? hideColState["EWMAHisVol"] : tableColList['EWMAHisVol'].hide),
+      PureFieldWithValueCheck("BSM_EWMAHisVol", tableColList.BSM_EWMAHisVol.text, 140, 2, "BSM_EWMAHisVol" in hideColState ? hideColState["BSM_EWMAHisVol"] : tableColList['BSM_EWMAHisVol'].hide),
+      PureFieldWithValueCheck("MC_EWMAHisVol", tableColList.MC_EWMAHisVol.text, 140, 2, "MC_EWMAHisVol" in hideColState ? hideColState["MC_EWMAHisVol"] : tableColList['MC_EWMAHisVol'].hide),
+      PureFieldWithValueCheck("BT_EWMAHisVol", tableColList.BT_EWMAHisVol.text, 140, 2, "BT_EWMAHisVol" in hideColState ? hideColState["BT_EWMAHisVol"] : tableColList['BT_EWMAHisVol'].hide),
+    ]
+  }
+
+
   const fetchOptionsData = useFetch({ cachePolicy: 'no-cache' })
 
   const renderOptionsData = (file_name) => {
@@ -29,6 +97,67 @@ const Options = ({loadingAnimeRef}) => {
     ]).then((allResponses) => {
       console.log(allResponses)
       if (allResponses.length == 1 && allResponses[0] !== null) {
+
+        // [{"symbol":"A","stockPrice":149.50999450683594,"EWMA_historicalVolatility":0.2519420533670158,"contracts":[{"expiryDate":"2022-01-21","calls":[{"lastTradeDate":"2022-01-12","strike":155.0,"lastPrice":0.32,"bid":0.35,"ask":0.5,"change":0.049999982,"percentChange":18.51851,"volume":30,"openInterest":721,"impliedVolatility":0.22461712890624996,"valuationData":{"BSM_EWMAHisVol":0.7042894690005248,"MC_EWMAHisVol":0.70279983534146,"BT_EWMAHisVol":0.7046023394736802}}],"puts":[]}]}
+        var calls = []
+        var puts = []
+        allResponses[0].forEach((data) => {
+          let symbol = data["symbol"]
+          let stock_price = data["stockPrice"]
+          let ewma_his_vol = data["EWMA_historicalVolatility"]
+          data["contracts"].forEach((contracts) => {
+            let expiry_date = contracts["expiryDate"]
+            let extra_data_func = (calls_puts)=>{
+              let output = calls_puts.map((cp, index) => {
+                let v = cp["valuationData"]
+                let o = {
+                  id: index,
+                  symbol: symbol,
+                  stockPrice: stock_price,
+                  EWMAHisVol: ewma_his_vol,
+                  expiryDate: expiry_date,
+                  lastTradeDate: cp["lastTradeDate"] !== undefined && cp["lastTradeDate"] !== null && cp["lastTradeDate"] !== '-' ? cp["lastTradeDate"] : -Number.MAX_VALUE,
+                  strike: cp["strike"] !== undefined && cp["strike"] !== null && cp["strike"] !== '-' ? cp["strike"] : -Number.MAX_VALUE,
+                  lastPrice: cp["lastPrice"] !== undefined && cp["lastPrice"] !== null && cp["lastPrice"] !== '-' ? cp["lastPrice"] : -Number.MAX_VALUE,
+                  bid: cp["bid"] !== undefined && cp["bid"] !== null && cp["bid"] !== '-' ? cp["bid"] : -Number.MAX_VALUE,
+                  ask: cp["ask"] !== undefined && cp["ask"] !== null && cp["ask"] !== '-' ? cp["ask"] : -Number.MAX_VALUE,
+                  change: cp["change"] !== undefined && cp["change"] !== null && cp["change"] !== '-' ? cp["change"] : -Number.MAX_VALUE,
+                  percentChange: cp["percentChange"] !== undefined && cp["percentChange"] !== null && cp["percentChange"] !== '-' ? cp["percentChange"] / 100.0 : -Number.MAX_VALUE,
+                  volume: cp["volume"] !== undefined && cp["volume"] !== null && cp["volume"] !== '-' ? cp["volume"] : -Number.MAX_VALUE,
+                  openInterest: cp["openInterest"] !== undefined && cp["openInterest"] !== null && cp["openInterest"] !== '-' ? cp["openInterest"] : -Number.MAX_VALUE,
+                  impliedVolatility: cp["impliedVolatility"] !== undefined && cp["impliedVolatility"] !== null && cp["impliedVolatility"] !== '-' ? cp["impliedVolatility"] : -Number.MAX_VALUE,
+                  BSM_EWMAHisVol: v["BSM_EWMAHisVol"] !== undefined && v["BSM_EWMAHisVol"] !== null && v["BSM_EWMAHisVol"] > 0 ? v["BSM_EWMAHisVol"] : -Number.MAX_VALUE,
+                  MC_EWMAHisVol: v["MC_EWMAHisVol"] !== undefined && v["MC_EWMAHisVol"] !== null && v["MC_EWMAHisVol"] > 0 ? v["MC_EWMAHisVol"] : -Number.MAX_VALUE,
+                  BT_EWMAHisVol: v["BT_EWMAHisVol"] !== undefined && v["BT_EWMAHisVol"] !== null && v["BT_EWMAHisVol"] > 0 ? v["BT_EWMAHisVol"] : -Number.MAX_VALUE,
+                }
+                let cnt = 0
+                let sum = 0
+                if (o.BSM_EWMAHisVol > 0) {
+                  cnt += 1
+                  sum += o.BSM_EWMAHisVol
+                }
+                if (o.MC_EWMAHisVol > 0) {
+                  cnt += 1
+                  sum += o.MC_EWMAHisVol
+                }
+                if (o.BT_EWMAHisVol > 0) {
+                  cnt += 1
+                  sum += o.BT_EWMAHisVol
+                }
+                o["avgEWMA"] = sum / cnt
+                o["priceBias"] = Math.abs(o.lastPrice - o["avgEWMA"]) / o.lastPrice
+                return o
+              })
+              return output
+            }
+            calls = calls.concat(extra_data_func(contracts["calls"]));
+            puts = puts.concat(extra_data_func(contracts["puts"]));
+          })
+        })
+        console.log(calls)
+        setCallsData(calls)
+        console.log(puts)
+        setPutsData(puts)
         loadingAnimeRef.current.setLoading(false)
       } else {
         console.error("renderOptionsData some data failed")
@@ -49,10 +178,11 @@ const Options = ({loadingAnimeRef}) => {
       renderOptionsData(name)
     }
   }
-
-  const [optionsTable, setOptionsTable] = useState()
+  const [callsData, setCallsData] = useState([])
+  const [putsData, setPutsData] = useState([])
+  const [hideColState, setHideColState] = useState({})
   const [arg, setArg] = useState(0)
-
+  
   useEffect(() => {
     // componentDidMount is here!
     // componentDidUpdate is here!
@@ -89,7 +219,20 @@ const Options = ({loadingAnimeRef}) => {
             </FormControl>
           </Grid>
         </Grid>
-        {optionsTable}
+        <div className={optionsStyle.table}>
+          <DataGrid rows={callsData} columns={genTableColTemplate()} rowsPerPageOptions={[]} autoPageSize={true} components={{ noRowsOverlay: DefaultDataGridTable, }} disableSelectionOnClick onColumnVisibilityChange={(param) => {
+            let tempHideColState = hideColState
+            tempHideColState[param['field']] = !param['isVisible']
+            setHideColState(tempHideColState)
+          }} />
+        </div>
+        <div className={optionsStyle.table}>
+          <DataGrid rows={putsData} columns={genTableColTemplate()} rowsPerPageOptions={[]} autoPageSize={true} components={{ noRowsOverlay: DefaultDataGridTable, }} disableSelectionOnClick onColumnVisibilityChange={(param) => {
+            let tempHideColState = hideColState
+            tempHideColState[param['field']] = !param['isVisible']
+            setHideColState(tempHideColState)
+          }} />
+        </div>
       </div>
       <ModalWindow modalWindowRef={modalWindowRef} />
     </div>
