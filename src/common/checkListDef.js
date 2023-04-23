aeab59d435451ad1008577e5202fce0f491b4574
zmcx16 <zmcx16@gmail.com>
2023-04-23 21:36:56 +0800

    implement checklist page

--- src/common/checkListDef.js
+++ src/common/checkListDef.js
@@ -7,8 +7,9 @@ export const ChecklistKey_Def = {
     "P/E":{
         "name": "P/E",
         "type": "from_end",
+        "description": "Price to Earnings (TTM)",
         "checkpoint": true,
-        "default": {
+        "checkpoint_comp": {
             "args_items": ["< 5", "< 10", "< 15", "< 20", "< 25", "< 30", "< 40", "< 50", "> 5", "> 10", "> 15", "> 20", "> 25", "> 30", "> 40", "> 50"],
             "default_index": 3
         }
@@ -16,8 +17,9 @@ export const ChecklistKey_Def = {
     "eps_analysis":{
         "name": "EPS Analysis",
         "type": "tags",
+        "description": "EPS data from the Analysis",
         "checkpoint": true,
-        "default": {
+        "checkpoint_comp": {
             "args_items": ["all_positive", "keep_growth"],
             "default_selects": ["all_positive"]
         }
@@ -25,12 +27,20 @@ export const ChecklistKey_Def = {
     "eps_financials":{
         "name": "EPS Financials",
         "type": "tags",
+        "description": "EPS data from the Financial Statements",
         "checkpoint": true,
-        "default": {
+        "checkpoint_comp": {
             "args_items": ["all_positive", "keep_growth"],
-            "default_selects": ["all_positive"]
+            "default_selects": ["all_positive", "keep_growth"]
         }
     }
 }
+
+export const CheckpointsKeyList = Object.keys(ChecklistKey_Def).reduce((acc, key, index) => {
+    if (ChecklistKey_Def[key].checkpoint) {
+      acc.push(key)
+    }
+    return acc
+}, [])
 
 export const ChecklistTooltips = "This is a tooltip"
--- src/components/checklist/checkpointPannel.js
+++ src/components/checklist/checkpointPannel.js
@@ -1,4 +1,4 @@
-import React, { useState }from 'react'
+import React, { useState, useRef, useEffect }from 'react'
 import Button from '@mui/material/Button'
 import Dialog from '@mui/material/Dialog'
 import DialogActions from '@mui/material/DialogActions'
@@ -16,16 +16,20 @@ import ListItemText from '@mui/material/ListItemText'
 import DoubleArrowIcon from '@mui/icons-material/DoubleArrow'
 import FormControl from '@mui/material/FormControl'
 import Select from '@mui/material/Select'
+import Chip from '@mui/material/Chip'
 import InputLabel from '@mui/material/InputLabel'
+import Typography from '@mui/material/Typography'
 import shortid from 'shortid'
 
-import { ChecklistKey_Def } from '../../common/checklistDef'
+import { EPSGrowthTagsDict } from '../../common/tagsDef'
+import { ChecklistKey_Def, CheckpointsKeyList } from '../../common/checklistDef'
 
 import checkpointPannelStyle from './checkpointPannel.module.scss'
 
 const CheckpointPannel = ({ChecklistRef}) => {
   const [open, setOpen] = useState(false)
   const [checkPointSelect, setCheckPointSelect] = useState(0)
+  const [checkPointComp, setCheckPointComp] = useState(<></>)
 
   const [setting, setSetting] = useState({
     checklistName: ""
@@ -46,6 +50,47 @@ const CheckpointPannel = ({ChecklistRef}) => {
     setOpen(false)
   }
 
+  const [tags, setTags] = useState({})
+  const tagsRef = useRef({})
+
+  useEffect(() => {
+    // componentDidMount is here!
+    // componentDidUpdate is here!
+    setCheckPointComp(
+      <>
+        <div style={{display: ChecklistKey_Def[CheckpointsKeyList[checkPointSelect]].type === "from_end" ? 'block' : 'none'}}>
+          {"from_end"}
+        </div>
+        <div key={shortid.generate()}  className={checkpointPannelStyle.tagContainer} style={{display: ChecklistKey_Def[CheckpointsKeyList[checkPointSelect]].type === "tags" ? 'block' : 'none'}}>
+          {
+            Object.entries(ChecklistKey_Def[CheckpointsKeyList[checkPointSelect]].type === "tags" ? tags : {}).map(([key, value, index]) => {
+              console.log(tags)
+              return <Chip
+                key={shortid.generate()} 
+                label={value.text}
+                style={{
+                  margin: '3px 5px', 
+                  color: tags[key].enabe ? '#fff':'rgba(0, 0, 0, 0.87)',
+                  backgroundColor: tags[key].enabe ? '#ff5722' : '#e0e0e0'
+                }}
+                onClick={() => {
+                  // let tags_temp = tags    // bug: this is a reference, not a copy, will not trigger setTags
+                  let tags_temp = {...tags}  // copy the object
+                  tags_temp[key].enabe = !tags_temp[key].enabe
+                  tagsRef.current = tags_temp
+                  setTags(tags_temp)
+                }} 
+              />
+            })
+          }
+        </div>
+      </>
+    )
+    return () => {
+      // componentWillUnmount is here!
+    }
+  }, [checkPointSelect, tags]) // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <div>
       <Dialog className={checkpointPannelStyle.container} open={open} onClose={handleCancelClose} fullWidth maxWidth="lg">
@@ -55,167 +100,187 @@ const CheckpointPannel = ({ChecklistRef}) => {
             {"Add/Delete your checkpoints on this checklist"}
           </DialogContentText>
             <div className={checkpointPannelStyle.queryPannel} >
-                <FormControl size="small" variant="outlined" className={checkpointPannelStyle.checkpointSelect}>
-                <InputLabel htmlFor="checkpoints-select">{'Checkpoints'}</InputLabel>
-                <Select
-                    native
-                    value={checkPointSelect}
-                    displayEmpty
-                    onChange={(event) => {
-                    setCheckPointSelect(event.target.value)
-                    }}
-                    label={'Checkpoints'}
-                >
-                    {
-                    Object.keys(ChecklistKey_Def).reduce((acc, key, index) => {
-                        if (ChecklistKey_Def[key].checkpoint) {
-                        acc.push(<option key={shortid.generate()} value={index}>{ChecklistKey_Def[key].name}</option>)
+              <FormControl size="small" variant="outlined" className={checkpointPannelStyle.checkpointSelect}>
+              <InputLabel htmlFor="checkpoints-select">{'Checkpoints'}</InputLabel>
+              <Select
+                  native
+                  value={checkPointSelect}
+                  displayEmpty
+                  onChange={(event) => {
+                    if (ChecklistKey_Def[CheckpointsKeyList[event.target.value]].type === "tags") {
+                      let tags_temp = {}
+                      ChecklistKey_Def[CheckpointsKeyList[event.target.value]].checkpoint_comp.args_items.forEach((key) => {
+                        tags_temp[key] = {
+                          text: EPSGrowthTagsDict[key],
+                          enabe: ChecklistKey_Def[CheckpointsKeyList[event.target.value]].checkpoint_comp.default_selects.includes(key)
                         }
-                        return acc
-                    }, [])
+                      })
+                      tagsRef.current = tags_temp
+                      setTags(tags_temp)
                     }
-                </Select>
-                </FormControl>
-                <div></div>
-                <div className={checkpointPannelStyle.addCheckpointBtn}>
-                <IconButton aria-label="addCheckpoint" style={{maxWidth: '70px', maxHeight: '70px', minWidth: '70px', minHeight: '70px'}}>
-                    <DoubleArrowIcon fontSize="inherit" style={{maxWidth: '50px', maxHeight: '50px', minWidth: '50px', minHeight: '50px'}} />
+                    setCheckPointSelect(event.target.value)
+                  }}
+                  label={'Checkpoints'}
+              >
+                {
+                  CheckpointsKeyList.map((key, index) => {
+                    return <option key={shortid.generate()} value={index}>{ChecklistKey_Def[key].name}</option>
+                  })
+                }
+              </Select>
+              </FormControl>
+              <div></div>
+              <div className={checkpointPannelStyle.addCheckpointBtn}>
+                <IconButton aria-label="addCheckpoint" style={{maxWidth: '70px', maxHeight: '70px', minWidth: '70px', minHeight: '70px'}} onClick={() => {
+                  if (ChecklistKey_Def[CheckpointsKeyList[checkPointSelect]].type === "tags") {
+                    console.log(tags)
+                  }
+                }}>
+                  <DoubleArrowIcon fontSize="inherit" style={{maxWidth: '50px', maxHeight: '50px', minWidth: '50px', minHeight: '50px'}} />
                 </IconButton>
-                </div>
-                <div></div>
-                <div className={checkpointPannelStyle.checkpointList}>
-                    <List  sx={{
-                        width: '100%',
-                        bgcolor: 'background.paper',
-                        position: 'relative',
-                        overflow: 'auto',
-                        maxHeight: 270,
-                        border: 1,
-                        borderColor: 'grey.500',
-                        borderRadius: 1,
-                        '& ul': { padding: 0 },
-                    }}>
-                        <ListItem
-                        secondaryAction={
-                            <IconButton edge="end" aria-label="delete">
-                            <DeleteIcon />
-                            </IconButton>
-                        }
-                        >
-                        <ListItemAvatar>
-                            <Avatar>
-                            <FolderIcon />
-                            </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary="Single-line item"
-                            secondary="Secondary text"
-                        />
-                        </ListItem>
-                        <ListItem
-                        secondaryAction={
-                            <IconButton edge="end" aria-label="delete">
-                            <DeleteIcon />
-                            </IconButton>
-                        }
-                        >
-                        <ListItemAvatar>
-                            <Avatar>
-                            <FolderIcon />
-                            </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary="Single-line item"
-                            secondary="Secondary text"
-                        />
-                        </ListItem>
-                        <ListItem
-                        secondaryAction={
-                            <IconButton edge="end" aria-label="delete">
-                            <DeleteIcon />
-                            </IconButton>
-                        }
-                        >
-                        <ListItemAvatar>
-                            <Avatar>
-                            <FolderIcon />
-                            </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary="Single-line item"
-                            secondary="Secondary text"
-                        />
-                        </ListItem>
-                        <ListItem
-                        secondaryAction={
-                            <IconButton edge="end" aria-label="delete">
-                            <DeleteIcon />
-                            </IconButton>
-                        }
-                        >
-                        <ListItemAvatar>
-                            <Avatar>
-                            <FolderIcon />
-                            </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary="Single-line item"
-                            secondary="Secondary text"
-                        />
-                        </ListItem>
-                        <ListItem
-                        secondaryAction={
-                            <IconButton edge="end" aria-label="delete">
-                            <DeleteIcon />
-                            </IconButton>
-                        }
-                        >
-                        <ListItemAvatar>
-                            <Avatar>
-                            <FolderIcon />
-                            </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary="Single-line item"
-                            secondary="Secondary text"
-                        />
-                        </ListItem>
-                        <ListItem
-                        secondaryAction={
-                            <IconButton edge="end" aria-label="delete">
-                            <DeleteIcon />
-                            </IconButton>
-                        }
-                        >
-                        <ListItemAvatar>
-                            <Avatar>
-                            <FolderIcon />
-                            </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary="Single-line item"
-                            secondary="Secondary text"
-                        />
-                        </ListItem>
-                        <ListItem
-                        secondaryAction={
-                            <IconButton edge="end" aria-label="delete">
-                            <DeleteIcon />
-                            </IconButton>
-                        }
-                        >
-                        <ListItemAvatar>
-                            <Avatar>
-                            <FolderIcon />
-                            </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary="Single-line item"
-                            secondary="Secondary text"
-                        />
-                        </ListItem>
-                    </List>
-                </div>
+              </div>
+              <div></div>
+              <div className={checkpointPannelStyle.checkpointList}>
+                  <List  sx={{
+                      width: '100%',
+                      bgcolor: 'background.paper',
+                      position: 'relative',
+                      overflow: 'auto',
+                      maxHeight: 270,
+                      border: 1,
+                      borderColor: 'grey.500',
+                      borderRadius: 1,
+                      '& ul': { padding: 0 },
+                  }}>
+                      <ListItem
+                      secondaryAction={
+                          <IconButton edge="end" aria-label="delete">
+                          <DeleteIcon />
+                          </IconButton>
+                      }
+                      >
+                      <ListItemAvatar>
+                          <Avatar>
+                          <FolderIcon />
+                          </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                          primary="Single-line item"
+                          secondary="Secondary text"
+                      />
+                      </ListItem>
+                      <ListItem
+                      secondaryAction={
+                          <IconButton edge="end" aria-label="delete">
+                          <DeleteIcon />
+                          </IconButton>
+                      }
+                      >
+                      <ListItemAvatar>
+                          <Avatar>
+                          <FolderIcon />
+                          </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                          primary="Single-line item"
+                          secondary="Secondary text"
+                      />
+                      </ListItem>
+                      <ListItem
+                      secondaryAction={
+                          <IconButton edge="end" aria-label="delete">
+                          <DeleteIcon />
+                          </IconButton>
+                      }
+                      >
+                      <ListItemAvatar>
+                          <Avatar>
+                          <FolderIcon />
+                          </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                          primary="Single-line item"
+                          secondary="Secondary text"
+                      />
+                      </ListItem>
+                      <ListItem
+                      secondaryAction={
+                          <IconButton edge="end" aria-label="delete">
+                          <DeleteIcon />
+                          </IconButton>
+                      }
+                      >
+                      <ListItemAvatar>
+                          <Avatar>
+                          <FolderIcon />
+                          </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                          primary="Single-line item"
+                          secondary="Secondary text"
+                      />
+                      </ListItem>
+                      <ListItem
+                      secondaryAction={
+                          <IconButton edge="end" aria-label="delete">
+                          <DeleteIcon />
+                          </IconButton>
+                      }
+                      >
+                      <ListItemAvatar>
+                          <Avatar>
+                          <FolderIcon />
+                          </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                          primary="Single-line item"
+                          secondary="Secondary text"
+                      />
+                      </ListItem>
+                      <ListItem
+                      secondaryAction={
+                          <IconButton edge="end" aria-label="delete">
+                          <DeleteIcon />
+                          </IconButton>
+                      }
+                      >
+                      <ListItemAvatar>
+                          <Avatar>
+                          <FolderIcon />
+                          </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                          primary="Single-line item"
+                          secondary="Secondary text"
+                      />
+                      </ListItem>
+                      <ListItem
+                      secondaryAction={
+                          <IconButton edge="end" aria-label="delete">
+                          <DeleteIcon />
+                          </IconButton>
+                      }
+                      >
+                      <ListItemAvatar>
+                          <Avatar>
+                          <FolderIcon />
+                          </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                          primary="Single-line item"
+                          secondary="Secondary text"
+                      />
+                      </ListItem>
+                  </List>
+              </div>
+              <div className={checkpointPannelStyle.checkpointDescription}>
+                <Typography variant="h6">
+                  {ChecklistKey_Def[CheckpointsKeyList[checkPointSelect]].description}
+                </Typography>
+              </div>
+              <div className={checkpointPannelStyle.checkPointComp}>
+                {checkPointComp}
+              </div>
             </div>
         </DialogContent>
         <DialogActions>
