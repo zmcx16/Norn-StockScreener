28ded547cf984eb2e8571e07619c6c2bb774687a
zmcx16 <zmcx16@gmail.com>
2024-02-28 03:07:29 +0800

    feat: implement benford law page

---
+++ src/components/stock-benford-law/stockBenfordLawChart.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {
+  LineChart,
+  ComposedChart,
+  Line,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from 'recharts'
+
+import moment from 'moment'
+
+const StockBenfordLawChart = ({ title }) => {
+  return (
+    <></>
+  )
+}
+
+
+export default StockBenfordLawChart
