48f04ba490a2fb250efbe1eb0ab248b59015ce3c
zmcx16 <zmcx16@gmail.com>
2024-10-08 00:59:03 +0800

    enhance UI

--- src/components/gpt-investing-assistant/companyAnalysisParam.js
+++ src/components/gpt-investing-assistant/companyAnalysisParam.js
@@ -28,7 +28,7 @@ const ITEM_PADDING_TOP = 8
 const MenuProps = {
   PaperProps: {
     style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      maxHeight: ITEM_HEIGHT * 13.5 + ITEM_PADDING_TOP,
       width: 250,
     },
   },
