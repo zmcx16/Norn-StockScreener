bab2daee95210684ff73469399e6074a186c44b7
zmcx16 <zmcx16@gmail.com>
2024-10-09 03:11:29 +0800

    support gpt o1-preview & mini module

--- src/common/gptdef.js
+++ src/common/gptdef.js
@@ -1,6 +1,6 @@
 export const CompanyAnalysisAPI = "/stock-minehunter/api/task/do-ai-company-analysis"
 
-export const GPTModelSelectDef = ['gpt-4o', 'gpt-4o-mini', 'gpt-4-turbo', 'gpt-3.5-turbo']
+export const GPTModelSelectDef = ['gpt-4o', 'gpt-4o-mini', 'o1-preview', 'o1-mini', 'gpt-4-turbo', 'gpt-3.5-turbo']
 export const AnalysisSelectDef = [
   {
     name: 'CompanyAnalysis',
